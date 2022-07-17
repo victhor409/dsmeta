ef32201e120b015a2dfb4070c49edb786fb2957e
jvictor <victhor409@gmail.com>
2022-07-16 22:18:40 -0300

    Other Components

--- frontend/frontend/src/App.tsx
+++ frontend/frontend/src/App.tsx
@@ -1,13 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+
 import NotificationButton from './components/NotificationButton'
+import Header from './components/Header'
+import SalesCard from './components/SalesCard'
 
 function App() {
   return(
     <>
-      <h1>Ola Mundo</h1>
-    <NotificationButton/>
+     <Header/>
+      <main>
+        <section id='sales'>
+          <div className='dsmeta-container'>
+            <SalesCard/>
+          </div>
+        </section>
+
+      </main>
     </>
     
   )
---
+++ frontend/frontend/src/components/Header/index.tsx
@@ -0,0 +1,19 @@
+import logo from '../../assets/img/logo.svg'
+import './styles.css'
+
+
+function Header(){
+    return(
+
+        <div className="dsmeta-logo-container">
+            <img src={logo} alt="DSMeta" />
+            <h1>DSMeta</h1>
+            <p>
+              Desenvolvido por
+              <a href="https://www.instagram.com/devsuperior.ig">@devsuperior.ig</a>
+            </p>
+        </div>
+    )
+}
+
+export default Header
